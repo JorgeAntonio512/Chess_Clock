5fbe8e7697d40f1782aae476462e68c7e1ec2552
JorgeAntonio512 <jorge.antonio.de.segundo@gmail.com>
2015-08-14 14:18:24 -0500

    got both total time clocks to work. Now need to add start/stop, delay, and pause when turn is over.

--- app/src/main/java/com/georgepazdral/chessclock/ChessClock.java
+++ app/src/main/java/com/georgepazdral/chessclock/ChessClock.java
@@ -1,9 +1,17 @@
 package com.georgepazdral.chessclock;
 
+import android.content.Context;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ChessClock extends AppCompatActivity {
 
@@ -11,6 +19,88 @@ public class ChessClock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_clock);
+
+        Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+
+        final TextView mTextField = (TextView) findViewById(R.id.textView);
+        final EditText mEditText = (EditText) findViewById(R.id.editText);
+        final TextView mTextField2 = (TextView) findViewById(R.id.textView2);
+        final EditText mEditText2 = (EditText) findViewById(R.id.editText2);
+
+
+
+        /*NumberPicker mNumberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+
+        String[] myStringArray = new String[]{"1","2","3","4","5","6","60"};
+        mNumberPicker.setMinValue(0);
+        mNumberPicker.setMaxValue(myStringArray.length - 1);
+        mNumberPicker.setDisplayedValues(myStringArray);
+
+
+        final long num = Long.parseLong(myStringArray[mNumberPicker.getValue()]);*/
+
+
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final long num = Long.parseLong(mEditText.getText().toString());
+                new CountDownTimer((num * 60000), 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        mTextField.setText("done!");
+                    }
+                }.start();
+
+
+                Context context = getApplicationContext();
+                CharSequence text = "the number selected is " + num;
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final long num = Long.parseLong(mEditText2.getText().toString());
+                new CountDownTimer((num * 60000), 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        mTextField2.setText("seconds remaining: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                        mTextField2.setText("done!");
+                    }
+                }.start();
+
+
+                Context context = getApplicationContext();
+                CharSequence text = "the number selected is " + num;
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
+
+
+
     }
 
     @Override
